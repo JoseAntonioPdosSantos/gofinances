5e1d21c4ac846307497a0f7885eb19e60a2bd92a
José Antonio Pinto dos Santos <jose.santos@ps.otg.com.br>
2022-04-15 13:39:40 -0300

    feat: styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import  * as React from 'react';
+import {  Container, Title } from './style';
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Title>Dashboard</Title>
+        </Container>
+    );
+}
