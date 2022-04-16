b3aa5aa670a20cd35f951ba33c1c06aada173716
José Antonio Pinto dos Santos <jose.santos@ps.otg.com.br>
2022-04-15 21:59:50 -0300

    feat: info user component add

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Container, Title } from './style';
+import { Text, View } from 'react-native';
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    User,
+    Photo,
+    UserGreeting,
+    UserName
+} from './style';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/12631140?v=4'}}/>
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>José Antonio</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container> 
     );
 }
